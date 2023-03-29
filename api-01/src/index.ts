5b5ffec28817b7055ef0326a6a4c7368c2614c53
lucioneves <luciocecilia20@gmail.com>
2023-03-29 13:57:00 -0400

    enviando para o sevidor

--- api-01/src/index.ts
+++ api-01/src/index.ts
@@ -4,7 +4,7 @@ import express, { Request, Response, NextFunction } from 'express';
 const app = express();
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Enviado com, sucesso!!!' });
+    res.status(200).send({ foo: 'Enviado com,  sucesso!!!' });
 });
 
 app.listen(3001, () => {
